ab8edb9dc6030c79279b06a5df6346241abff0c1
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2025-04-30 18:54:38 +0300

    update install.sh

--- install.sh
+++ install.sh
@@ -120,7 +120,8 @@ forge --version
 # === Install Contract Dependencies ===
 #cd $HOME/infernet-container-starter/projects/hello-world/contracts/lib/
 cd $HOME/infernet-container-starter/projects/hello-world/contracts/
-rm -rf forge-std infernet-sdk
+rm -rf lib
+#rm -rf forge-std infernet-sdk
 forge install  foundry-rs/forge-std
 forge install  ritual-net/infernet-sdk
 
